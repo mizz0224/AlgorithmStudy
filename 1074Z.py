aea694e7b264f5005c9c421e58268ac41f4a6643
mizz0224 <61971962+mizz0224@users.noreply.github.com>
2022-01-13 23:56:23 +0900

    01.12

---
+++ 1074Z.py
@@ -0,0 +1,28 @@
+import sys
+
+num = -1
+n, r, c = map(int, sys.stdin.readline().split())
+
+
+def recursive(x, y, N):
+    global num
+
+    if N == 2:
+        for i in range(x, x + N):
+            for j in range(y, y + N):
+                num += 1
+                if i == r and j == c:
+                    print(num)
+                    exit(0)
+        return
+
+    if not (x <= r < x + N and y <= c < y + N):
+        num += N * N
+        return
+
+    for i in range(x, x + N, N // 2):
+        for j in range(y, y + N, N // 2):
+            recursive(i, j, N // 2)
+
+
+recursive(0, 0, 2 ** n)
